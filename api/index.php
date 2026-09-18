a4760204024cac1bb354fc0f398d002ce228d9a9
Krisnaadiguna <krisnaaddd026@gmail.com>
2026-09-18 15:52:43 +0800

    Fix storage paths untuk serverless Vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,19 @@
 <?php
+
+// Siapkan folder writeable sementara untuk storage dan cache di Vercel
+$dirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
+// Teruskan request ke public/index.php bawaan Laravel
 require __DIR__ . '/../public/index.php';
